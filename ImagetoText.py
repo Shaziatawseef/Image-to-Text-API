de0586f958f7b51e00a448065af8a08becf886ea
Shaziatawseef <shaziatawseef786@gmail.com>
2025-09-03 22:34:58 +0530

    Update ImagetoText.py

--- ImagetoText.py
+++ ImagetoText.py
@@ -58,8 +58,4 @@ if __name__ == "__main__":
     # Render expects PORT from environment
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)        }), 500
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port)
